10d133a265ce8833b47681ca8d3cd0379a9765c1
AlejandroASP <alejandroasp71@gmail.com>
2024-01-12 11:43:17 +0000

    cambios ejercicio

--- GaleriaImagenesSerafin/src/main/java/es/cifpcm/GaleriaImagenesSerafin/ImageManagerServlet.java
+++ GaleriaImagenesSerafin/src/main/java/es/cifpcm/GaleriaImagenesSerafin/ImageManagerServlet.java
@@ -5,23 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
 public class ImageManagerServlet {
 
-    private static String UPLOAD_FOLDER = "F:\\DSW\\Java\\GaleriaImagenesSerafin\\src\\main\\resources\\uploadsSerafin\\";
-
+    private String UPLOAD_FOLDER = new File(System.getProperty("user.dir"), "uploadsSerafin").getAbsolutePath();
     @PostMapping("/imageManager")
     public String upload(@RequestParam("imageFile") MultipartFile file, RedirectAttributes redirectAttributes) {
         try {
@@ -30,7 +29,7 @@ public class ImageManagerServlet {
             if (! directory.exists()){
                 directory.mkdir();
             }
-            Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
+            Path path = Paths.get(UPLOAD_FOLDER, file.getOriginalFilename());
             Files.write(path, bytes);
             redirectAttributes.addFlashAttribute("message", "Archivo subido correctamente");
             return "redirect:/index";
@@ -39,20 +38,9 @@ public class ImageManagerServlet {
             return "redirect:/index";
         }
     }
-    @GetMapping("/imageManager")
-    public @ResponseBody List<String> listImages() {
-        File folder = new File(UPLOAD_FOLDER);
-        File[] listOfFiles = folder.listFiles();
-        List<String> imageLinks = new ArrayList<>();
-        for (File file : listOfFiles) {
-            if (file.isFile()) {
-                imageLinks.add("/uploadsSerafin/" + file.getName());
-            }
-        }
-        return imageLinks;
-    }
-    @GetMapping("/index")
-    public String index(Model model) {
+
+    @GetMapping({"/imageManager", "/index"})
+    public String listImages(Model model) {
         File folder = new File(UPLOAD_FOLDER);
         File[] listOfFiles = folder.listFiles();
 
